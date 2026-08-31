c1521fe42bd46f50e4bf38a45feec6fef427a211
suvasis-patra <suvasis02@kgpian.iitkgp.ac.in>
2026-08-31 13:52:59 +0530

    new commit

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,5 +1,10 @@
 const About = () => {
-  return <div>About,Hi there wow! The day is not going great.</div>;
+  return (
+    <div>
+      About,Hi there wow! The day is not going great. Something very fishy going
+      on
+    </div>
+  );
 };
 
 export default About;
