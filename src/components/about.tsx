137905022c5462afe8f4060529f30807d1e410eb
suvasis-patra <suvasis02@kgpian.iitkgp.ac.in>
2026-08-31 17:15:29 +0530

    fix issue

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,5 +1,10 @@
 const About = () => {
-  return <div>Now lets see if it works. Maybe this will work now</div>;
+  return (
+    <div>
+      Now lets see if it works. Maybe this will work now. At last i have fixed
+      the issue
+    </div>
+  );
 };
 
 export default About;
